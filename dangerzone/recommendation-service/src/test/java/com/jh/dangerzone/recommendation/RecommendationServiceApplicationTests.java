831530c407f1374d0a716c9ef64982e2c8963f80
Johannes9108 <johannes91.hedman@gmail.com>
2022-03-31 13:33:42 +0200

    Multi Project Repo
    Gradle build with multiproject setup

---
+++ dangerzone/recommendation-service/src/test/java/com/jh/dangerzone/recommendation/RecommendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jh.dangerzone.recommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
